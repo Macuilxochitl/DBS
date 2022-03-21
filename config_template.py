7c345771e455c62cbcc84a35453bc6f50f935989
Zek Huang <zek.huang@outlook.com>
2022-03-21 12:32:43 +0800

    Add template config.

---
+++ config_template.py
@@ -0,0 +1,18 @@
+CENTRAL_NODE_ADDRESS = "a.com:5000"
+NODE_CHECK_INTERVAL = 5
+
+
+IS_CENTRAL_NODE = True
+
+NODE_NAME = "X"
+NODE_PORT = 5000
+
+NODE_ADDRESS = "a.com:{}".format(NODE_PORT)
+
+db_config = {
+    "db_name": "xxx",
+    "db_user": "xxx",
+    "db_password": "xxx",
+    "db_host": "localhost",
+    "db_port": 3306,
+}
